ba6105e62b5e0e043778a018b3e0ca2844cc3f80
Ghiga Florian-Codrut <ghigafloriancodrut@gmail.com>
2020-03-16 16:09:25 +0200

    init commit

---
+++ T3/T3/Source.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+void int2bit(int n, int*nr)
+{
+	int V[] = { 0,0,0,0,0,0,0,0 };
+	int i, m;
+	i = 0; m = 0;
+	while (n != 0)
+	{
+		if (n % 2 == 1) { m++; *nr = m; }
+		V[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+	for (i = 7; i >= 0; i--)
+	{
+		if (i == 3)printf(" ");
+		printf("%d", V[i]);
+	}
+	printf("\n");
+
+}
+
+int main()
+{
+	int N, i, nr, S;
+	nr = 0; S = 0;
+	printf("Introduceti un numar: "); scanf("%d", &N);
+	for (i = 1; i <= N; i++)
+	{
+		printf("%d - ", i); int2bit(i, &nr);
+		S += nr;
+	}
+	printf("Rezultat: %d\n", S);
+
+
+	system("pause");
+	return 0;
+}
